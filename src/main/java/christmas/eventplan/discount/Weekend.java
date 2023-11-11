54b302f3f4ab9c348ecd2f01d1fce697e2ceb579
constant94-dev <constant94@naver.com>
2023-11-11 21:21:29 +0900

    feat(eventplan): 주말 할인 메서드 및 테스트 코드 구현

--- src/main/java/christmas/eventplan/discount/Weekend.java
+++ src/main/java/christmas/eventplan/discount/Weekend.java
@@ -1,8 +1,16 @@
 package christmas.eventplan.discount;
 
+import christmas.constant.WeekendEnum;
+import java.util.Arrays;
+
 public class Weekend {
 
-    public void discountOnMainMenu() {
+    public int discountOnMainMenu(String currentDate) {
+        WeekendEnum weekend = Arrays.stream(WeekendEnum.values())
+                .filter(d -> d.getDay().equals(currentDate))
+                .findFirst()
+                .orElse(WeekendEnum.DAY_NOT_EXIST);
 
+        return weekend.getDiscount();
     }
 }
---
+++ src/test/java/christmas/eventplan/discount/WeekendTest.java
@@ -0,0 +1,36 @@
+package christmas.eventplan.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.constant.WeekendEnum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class WeekendTest {
+
+    @DisplayName("주말 이벤트 기간에 맞는 할인 금액 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2023", "2,2023", "8,2023", "9,2023", "15,2023", "16,2023", "22,2023", "23,2023", "29,2023",
+            "30,2023"})
+    void discountOnMainMenu(String currentDate, int expected) {
+        Weekend weekend = new Weekend();
+
+        int actual = weekend.discountOnMainMenu(currentDate);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("주말 이벤트 기간에 해당하지 않는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "4", "5", "6", "7", "10", "11", "12", "13", "14", "17", "18", "19", "20", "21", "24",
+            "25", "26", "27", "28", "31"})
+    void discountOnOrderNotInWeekend(String currentDate) {
+        Weekend weekend = new Weekend();
+
+        int actual = weekend.discountOnMainMenu(currentDate);
+
+        assertThat(actual).isEqualTo(WeekendEnum.DAY_NOT_EXIST.getDiscount());
+    }
+}
