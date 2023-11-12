ee6c0caa5ed4016612c912c6bb84d0845bd6f7f6
constant94-dev <constant94@naver.com>
2023-11-12 11:15:39 +0900

    feat(eventplan): 증정 이벤트 메서드 및 테스트 코드 구현

--- src/main/java/christmas/eventplan/Gift.java
+++ src/main/java/christmas/eventplan/Gift.java
@@ -1,8 +1,16 @@
 package christmas.eventplan;
 
+import christmas.constant.GiftEnum;
+import java.util.Arrays;
+
 public class Gift {
 
-    public void giftOnChampagne() {
+    public int giftOnChampagne(int totalAmount) {
+        GiftEnum gift = Arrays.stream(GiftEnum.values())
+                .filter(g -> g.getTotalAmount() <= totalAmount)
+                .findFirst()
+                .orElse(GiftEnum.GIFT_NOT_CHAMPAGNE);
 
+        return gift.getChampagne();
     }
 }
