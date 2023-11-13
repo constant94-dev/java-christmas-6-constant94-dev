f332ad2bcecbc53727e76db23fdfa66e9a213a90
constant94-dev <constant94@naver.com>
2023-11-14 05:34:15 +0900

    feat(view): 고객 입력 UI 관련 메서드 구현 및 출력 UI 스켈레톤 코드 생성

---
+++ src/main/java/christmas/eventplan/view/OutputView.java
@@ -0,0 +1,38 @@
+package christmas.eventplan.view;
+
+import static christmas.constant.view.EventPlanner.PLANNER_PREVIEW;
+
+public class OutputView {
+
+    public void printWooTecoEventPreview(String visitDate) {
+        System.out.printf((PLANNER_PREVIEW.getPlanner()) + "%s", visitDate);
+    }
+
+    public void printOrderToMenu() {
+
+    }
+
+    public void printTotalOrderAmountBeforeDiscount() {
+
+    }
+
+    public void printGiftToMenu() {
+
+    }
+
+    public void printDiscountDetails() {
+
+    }
+
+    public void printTotalDiscount() {
+
+    }
+
+    public void printEstimatedAmountAfterDiscount() {
+
+    }
+
+    public void printEventBadge() {
+
+    }
+}
