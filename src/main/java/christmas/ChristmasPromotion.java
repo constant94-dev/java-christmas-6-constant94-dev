25dc2baa70d4b6d762927cce2012c2cf70edc900
constant94-dev <constant94@naver.com>
2023-11-15 21:49:21 +0900

    feat(christmas): 크리스마스 프로모션 기능 출발지 메서드 구현

--- src/main/java/christmas/ChristmasPromotion.java
+++ src/main/java/christmas/ChristmasPromotion.java
@@ -1,8 +1,49 @@
 package christmas;
 
+import christmas.eventplan.badge.EventBadge;
+import christmas.eventplan.discount.Benefit;
+import christmas.eventplan.view.InputView;
+import christmas.eventplan.view.OutputView;
+import christmas.model.OrderInfo;
+import christmas.model.UserInfo;
+
 public class ChristmasPromotion {
+    private UserInfo userInfo;
+    private OrderInfo orderInfo;
+    private Benefit benefit;
+    private EventBadge eventBadge;
+
+    private InputView inputView;
+    private OutputView outputView;
 
     public void run() {
-        // 실제 로직 구현
+        ready();
+        promotionPlay();
+    }
+
+    private void ready() {
+        userInfo = new UserInfo();
+        orderInfo = new OrderInfo();
+        benefit = new Benefit();
+        eventBadge = new EventBadge();
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    private void promotionPlay() {
+        inputView.helloWooTeco();
+        inputView.visitToDate(userInfo);
+        inputView.orderToMenuAndCount(userInfo, orderInfo);
+
+        benefit.createEventBenefit(userInfo, eventBadge);
+
+        outputView.printWooTecoEventPreview(userInfo.getVisitDate());
+        outputView.printOrderToMenu(userInfo.getMenuOrders());
+        outputView.printTotalOrderAmountBeforeDiscount(userInfo.getEventDetails());
+        outputView.printGiftToMenu(userInfo.getEventDetails());
+        outputView.printDiscountDetails(userInfo.getEventDetails());
+        outputView.printTotalDiscount(userInfo.getEventDetails());
+        outputView.printEstimatedAmountAfterDiscount(userInfo.getEventDetails());
+        outputView.printEventBadge(eventBadge.getBadgeStatus());
     }
 }
